25e333d679081c1f7b6b906ae4c7696ccb3c2877
Ali Enver <alienver.sevgi@ingamegroup.com>
2017-06-22 14:36:24 +0300

    proje

---
+++ CloudTest/Assets/New Folder/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+
+    private int number;
+    public Text NumberText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(Timer());
+	}
+	
+    private IEnumerator Timer()
+    {
+        while (true)
+        {
+            NumberText.text = number.ToString();
+            yield return new WaitForSeconds(1.0f);
+            number++;
+        }
+    }
+
+}
